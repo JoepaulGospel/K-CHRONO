5d7d60a4dac29a39ebbb7ffa0081e76a2ea875fe
JoepaulGospel <162193500+JoepaulGospel@users.noreply.github.com>
2026-01-09 15:31:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,8 @@
-const CACHE_NAME = 'kchrono-v1';
-const assets = ['./', './index.html'];
-self.addEventListener('install', (event) => {
-event.waitUntil(
-caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
-);
+const CACHE_NAME = 'k-chrono-v2';
+const assets = ['/'];
+self.addEventListener('install', (e) => {
+e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
 });
-self.addEventListener('fetch', (event) => {
-event.respondWith(
-fetch(event.request).catch(() => caches.match(event.request))
-);
+self.addEventListener('fetch', (e) => {
+e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
